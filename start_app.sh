54052c274548a57dad248c89f05595ffc295e06a
Teodora <tedyted26@hotmail.com>
2024-10-11 10:34:25 +0200

    update on start_app

--- start_app.sh
+++ start_app.sh
@@ -5,9 +5,14 @@ function exit_err {
     exit 1
 }
 
-while getopts ":d:i:c:h:e:xg:f:p:u:s:n:l:m:" option; do
+while getopts ":i:c:n:" option; do
     case "$option" in
         i) INITIALIZATION="$OPTARG" ;;
+        c) CONFIGURATION="$OPTARG" ;;
+        # Notebook folder. By default /work/notebooks, but user can specify.
+        # This is different from "Select folders to use", 
+        # since it changes the configuration file so Polynote can detect the folder and show it in the interface
+        n) NB_DIR="$OPTARG" ;;
         :) exit_err "Missing argument for -$OPTARG" ;;
         *) exit_err "Invalid option -$OPTARG" ;;
     esac
@@ -18,12 +23,6 @@ if [[ -f "$INITIALIZATION" ]]; then
     printf "Running Initialization\n"
     printf "======================\n\n"
     case "$INITIALIZATION" in
-        *.txt)
-            pip install --user -r "$INITIALIZATION" || exit_err "Failed to install packages from $INITIALIZATION"
-            ;;
-        *.yml|*.yaml)
-            conda env update --file "$INITIALIZATION" || exit_err "Failed to update environment using $INITIALIZATION"
-            ;;
         *.sh)
             bash "$INITIALIZATION" || exit_err "Failed to execute script $INITIALIZATION"
             ;;
@@ -31,7 +30,28 @@ if [[ -f "$INITIALIZATION" ]]; then
             exit_err "File format not correct. Initialization must be specified in a *.txt, *.yml/yaml, or *.sh file."
             ;;
     esac
-fi    
+fi  
+
+if [[ -f "$CONFIGURATION" ]]; then
+    printf "\n======================\n"
+    printf "Adding Configuration\n"
+    printf "======================\n\n"
+    case "$CONFIGURATION" in
+        *.yml)
+        # The if can be removed.
+            if grep -q -E '^listen:\s*$' config.yml && \
+               grep -q -E '^\s+host:\s+0\.0\.0\.0\s*$' config.yml && \
+               grep -q -E '^\s+port:\s+8192\s*$' config.yml; then
+                cp "$CONFIGURATION" /opt/polynote/config.yml || exit_err "Failed to add configuration from $CONFIGURATION"
+            else
+                echo "Host or port listen configuration is not correctly set."
+            fi
+            ;;
+        *)
+            exit_err "File format not correct. Configuration must be specified in a *.yml file."
+            ;;
+    esac
+fi 
 
 # Launch Polynote
 printf "\n======================\n"
